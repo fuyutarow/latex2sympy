646582bbc5e236ff7f7b335f711d733c91cd13ad
fytroo <fytro10@gmail.com>
2018-01-07 06:00:11 +0900

    pip install gen

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import re
 from setuptools import setup
 
 try:
@@ -5,6 +7,14 @@ try:
         long_description = f.read()
 except:
     long_description = ''
+
+d_include_dir = []
+for root, folders, files in os.walk("latex2sympy/gen"):
+    buff = []
+    d_include_dir.append(
+        re.sub("\\\\", "/", re.sub("latex2sympy/", "", root)) + "/*.*")
+    for f in files:
+        buff.append(f)
 
 setup(
     name='latex2sympy3',
@@ -15,6 +25,6 @@ setup(
     maintainer_email='jack@bancast.net',
     url='https://github.com/jackatbancast/latex2sympy',
     packages=['latex2sympy'],
+    package_data={'latex2sympy': d_include_dir},
     license='MIT',
-    install_requires=['antlr4-python3-runtime==4.5.3', 'sympy']
-)
+    install_requires=['antlr4-python3-runtime==4.5.3', 'sympy'])
